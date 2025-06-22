5c277d38c58e7dc385d43f850f8c647b4fa6b2d2
Gabriel Simon <gabrielsimon775@gmail.com>
2025-06-22 14:46:08 -0300

    estrutura do projeto

---
+++ backend/gerenciamento-cursos/src/test/java/br/com/edtech/gerenciamento_cursos/GerenciamentoCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edtech.gerenciamento_cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
