370c75fd4d9f0095f305ca4f0131daf21e32302d
GABRIEL SIMON <gabriel_simon1@estudante.sc.senai.br>
2025-06-25 20:15:46 -0300

    segundo teste add

--- backend/gerenciamento-cursos/src/test/java/br/com/edtech/gerenciamento_cursos/services/ServiceTest.java
+++ backend/gerenciamento-cursos/src/test/java/br/com/edtech/gerenciamento_cursos/services/ServiceTest.java
@@ -6,7 +6,13 @@ import org.junit.jupiter.api.Test;
 public class ServiceTest {
 
     @Test
-    void testSoma() {
+    void testSomaValoresComSucesso() {
         Assertions.assertNotNull(Service.somaValores(10.0, 10.0));
+    }
+
+    @Test
+    void testSomaValoresComSucesso2() {
+        Double result = Service.somaValores(10.0, 10.0);
+        Assertions.assertEquals(20, result);
     }
 }
