5e8d722e733815fbea9803caf937bdeba8c8f796
gabrielsimon445 <gabriel_simon1@estudante.sc.senai.br>
2025-06-25 20:55:49 -0300

    Update GerenciamentoCursosApplicationTests to specify application class in SpringBootTest annotation

--- backend/gerenciamento-cursos/src/test/java/br/com/edtech/gerenciamento_cursos/GerenciamentoCursosApplicationTests.java
+++ backend/gerenciamento-cursos/src/test/java/br/com/edtech/gerenciamento_cursos/GerenciamentoCursosApplicationTests.java
@@ -3,11 +3,11 @@ package br.com.edtech.gerenciamento_cursos;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class GerenciamentoCursosApplicationTests {
+@SpringBootTest(classes = GerenciamentoCursosApplication.class)
+public class GerenciamentoCursosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Testa se o contexto carrega corretamente
+    }
 }
